9b2b44cf9b86d687dc7120a64c2892d18a580083
Atul Patil <atul550@gmail.com>
2018-08-21 14:28:45 -0400

    dynamic loading side menu bar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,33 +2,19 @@
   <div class="sidebar">
    <a-layout-content style="padding: 0 50px">
       <a-layout style="padding: 24px 0; background: #fff">
-        <a-layout-sider width="200" style="background: #fff">
+        <a-layout-sider width="100%" style="background: #fff">
           <a-menu
             mode="inline"
             :defaultSelectedKeys="['1']"
             :defaultOpenKeys="['sub1']"
             style="height: 100%"
           >
-            <a-sub-menu key="sub1">
-              <span slot="title"><a-icon type="user" />subnav 1</span>
-              <a-menu-item key="1">option1</a-menu-item>
-              <a-menu-item key="2">option2</a-menu-item>
-              <a-menu-item key="3">option3</a-menu-item>
-              <a-menu-item key="4">option4</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub2">
-              <span slot="title"><a-icon type="laptop" />subnav 2</span>
-              <a-menu-item key="5">option5</a-menu-item>
-              <a-menu-item key="6">option6</a-menu-item>
-              <a-menu-item key="7">option7</a-menu-item>
-              <a-menu-item key="8">option8</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub3">
-              <span slot="title"><a-icon type="notification" />subnav 3</span>
-              <a-menu-item key="9">option9</a-menu-item>
-              <a-menu-item key="10">option10</a-menu-item>
-              <a-menu-item key="11">option11</a-menu-item>
-              <a-menu-item key="12">option12</a-menu-item>
+            <a-sub-menu v-for="section in sections" :key="section.name">
+              <span slot="title"><a-icon type="user"/>{{section.name}}</span>
+              <a-sub-menu v-for="endPoint in section.endPoints" :key="endPoint.name">
+                <span slot="title"><a-icon type="laptop" />{{endPoint.name}}</span>
+                <a-menu-item v-for="operation in endPoint.operations" :key="operation.name">{{operation.name}}</a-menu-item>
+              </a-sub-menu>
             </a-sub-menu>
           </a-menu>
         </a-layout-sider>
@@ -39,3 +25,19 @@
     </a-layout-content>
   </div>
 </template>
+<script>
+import sideBarJson from '../data/sidebar-data.json'
+export default {
+  data () {
+    return {
+      sections: sideBarJson
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+.navbar {
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.2);
+}
+</style>
